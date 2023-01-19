b635079dbd969a77342e75b67b98724303d8cb7b
Rahmandani Winarta <88223348+rahmandaniwinarta@users.noreply.github.com>
2023-01-19 16:41:37 +0700

    FEAT : CHECKOUT-ADDRESS GR5-22

--- projects/server/models/product.js
+++ projects/server/models/product.js
@@ -12,14 +12,16 @@ module.exports = (sequelize, DataTypes) => {
       Product.hasMany(models.Product_Image, {
         foreignKey: "IdProduct",
       });
-      Product.hasMany(models.Cart);
+      Product.hasMany(models.Cart, {
+        foreignKey: "IdProduct",
+      });
       Product.hasMany(models.Journal);
       Product.hasOne(models.Stock_Mutation);
       Product.belongsToMany(models.Warehouse, {
         through: "Product_Warehouses",
       });
       Product.hasMany(models.Product_Warehouses, { as: "Details" });
-      Product.belongsTo(models.Product_Category)
+      Product.belongsTo(models.Product_Category);
     }
   }
   Product.init(
