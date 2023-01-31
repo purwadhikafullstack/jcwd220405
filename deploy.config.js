27b23a99a7d09e4b93880a485f4ee02cf148a468
Purwadhika Fullstack <93514408+purwadhikafullstack@users.noreply.github.com>
2023-01-31 10:36:59 +0700

    Update deploy.config.js

--- deploy.config.js
+++ deploy.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: "JCWD-2000-01", // Format JCWD-{batchcode}-{groupnumber}
+      name: "JCWD-2204-05", // Format JCWD-{batchcode}-{groupnumber}
       script: "./projects/server/src/index.js",
       env: {
         NODE_ENV: "production",
-        PORT: 8000,
+        PORT: 8405,
       },
       time: true,
     },
